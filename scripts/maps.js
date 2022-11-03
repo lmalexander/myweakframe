3387c042ca75c1b1fdf9f491d76f6acc1dfa77bb
lmalexander <42302324+lmalexander@users.noreply.github.com>
2022-11-03 15:34:10 -0400

    added main my weak frame map

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,18 +1,31 @@
 console.log ("maps.js is connected");
 
 // *** MAIN MAP ***
-let mapMain = L.map('main-map').setView({lon: 0, lat: 0}, 2);
+// initialize Leaflet
+let mapMain = L.map('main-map').setView({lon: -77.036873, lat: 38.907192}, 5);
 
-      // add the OpenStreetMap tiles
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-      }).addTo(mapMain);
+function mapMainReturn() {
 
-      // show the scale bar on the lower left corner
-      L.control.scale({imperial: true, metric: true}).addTo(mapMain);
+  console.log("function button worked");
+  // add the OpenStreetMap tiles
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+  }).addTo(mapMain);
 
-      // show a marker on the map
-      L.marker({lon: 0, lat: 0}).bindPopup('The center of the world').addTo(mapMain);
+  // show the scale bar on the lower left corner
+  L.control.scale({imperial: true, metric: true}).addTo(mapMain);
 
-      document.getElementById('main-map').appendChild = mapMain;
+  // show a marker on the map
+  L.marker({lon: -72.519852, lat: 42.373222}).bindPopup('Amherst, MA').addTo(mapMain);
+  L.marker({lon: -72.673363, lat: 41.765770}).bindPopup('<a href="./../pages/main-map/hartford.html">Hartford, CT</a>').addTo(mapMain);
+  L.marker({lon: -75.165222, lat: 39.952583}).bindPopup('<a href="./../pages/main-map/philadelphia.html">Phihladelphia, PA</a>').addTo(mapMain);
+  L.marker({lon: -77.036873, lat: 38.907192}).bindPopup('<a href="./../pages/main-map/washington-dc.html">Washington, DC</a>').addTo(mapMain);
+  L.marker({lon: -77.436050, lat: 37.540726}).bindPopup('<a href="./../pages/main-map/richmond.html">Richmond, VA</a>').addTo(mapMain);
+  L.marker({lon: 76.6090128, lat: 36.0579196}).bindPopup('<a href="./../pages/main-map/edenton.html">Edenton, NC</a>').addTo(mapMain);
+  L.marker({lon: -76.6108732, lat: 36.0583529}).bindPopup('6 W. King St.').addTo(mapMain);
+
+}
+
+
+
